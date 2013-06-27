f9e415534e3cfbfaf667f46b4dd27e0847e1bb55
birkhoff <mike.birkhoff@gmx.de>
2013-06-27 12:06:56 +0200

    updated escaper

--- prolog-Doc/escaper.pl
+++ prolog-Doc/escaper.pl
@@ -1,4 +1,4 @@
-:- module(escaper,[escaping_format/2]).
+:- module(escaper,[escaping_format/2, escape_single_argument/2]).
 
 :- use_module(library(lists)).
 :- use_module(library(codesio)).
@@ -19,3 +19,15 @@ escape_codes_list([34|T],[92,34|T2]) :-
     escape_codes_list(T,T2).
 escape_codes_list([H|T],[H|T2]) :-
     escape_codes_list(T,T2).
+
+
+escape_single_argument(Arg,EscArg) :-
+    format_to_codes('~w', [Arg], Codes),
+    escape_sequence_list(Codes,EscCodes),
+    atom_codes(EscArg,EscCodes).
+
+escape_sequence_list([],[]).
+escape_sequence_list([60|T],[38,108,116,59|T2]) :-
+	escape_sequence_list(T,T2).
+escape_sequence_list([H|T],[H|T2]) :-
+    escape_sequence_list(T,T2).
