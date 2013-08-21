ec7e289adf49c31d24f220c10e1670ba8288beed
birkhoff <papstVII@gmx.de>
2013-08-21 17:47:42 +0200

    enhenced codeq analyzer, no duplicate calls

--- prolog-Doc/codeq_analyzer.pl
+++ prolog-Doc/codeq_analyzer.pl
@@ -14,7 +14,7 @@
 :- op(300, fy, ~~).
 
 
-:- dynamic exports/3, imports/3, imports/1, predicates/7, dynamics/1, metas/2, volatiles/1, multifiles/1,in_module/1, in_clause/2, module_pos/2, ops/3, blocking/2, modes/2, stream/1. 
+:- dynamic exports/3, imports/3, imports/1, predicates/8, dynamics/1, metas/2, volatiles/1, multifiles/1,in_module/1, in_clause/2, module_pos/2, ops/3, blocking/2, modes/2, stream/1. 
 
 in_module('user').
 module_pos(1,1).
@@ -87,14 +87,22 @@ write_metas(Name/Ar) :-
 	fail.
 write_metas(_Name/_Ar).
 
-write_predicates :-
-    findall(pr(Name,Ar), predicates(Name,Ar,_,_,_,_,_), ListOfNames),
-    remove_dups(ListOfNames,ListOfNames2),
-    write_predicates(ListOfNames2).
 write_predicates([]).
 write_predicates([pr(Name,Ar)|Names]) :-
-    write_predicates2(Name,Ar,[],[],[],[],0),
+    write_predicates2(_,Name,Ar,[],[],[],[],0),
     write_predicates(Names).
+
+% write_predicates Module writes predicates of a given module
+
+write_predicates(Module) :-		
+    findall(pr(Name,Ar), predicates(Module,Name,Ar,_,_,_,_,_), ListOfNames),
+    remove_dups(ListOfNames,ListOfNames2),
+    write_predicates(ListOfNames2).
+write_predicates :-
+    findall(pr(Name,Ar), predicates(_,Name,Ar,_,_,_,_,_), ListOfNames),
+    remove_dups(ListOfNames,ListOfNames2),
+    write_predicates(ListOfNames2).
+
 
 bind_args(Args,VC,VCN) :-
     term_variables(Args,Variables),
@@ -109,7 +117,7 @@ bind_args2([V|Vs],VC,VCN) :-
 
 is_dynamic(Name,Ar,'<dynamic>true</dynamic>') :- dynamics(Name/Ar), !.
 is_dynamic(_Name,_Ar,'<dynamic>false</dynamic>').
-is_meta(Name,Ar,Return) :- metas(Name/Ar, Args), format_to_codes('<meta>true</meta>', [], Codes), atom_codes(Return, Codes), !.
+is_meta(Name,Ar,Return) :- metas(Name/Ar, _Args), format_to_codes('<meta>true</meta>', [], Codes), atom_codes(Return, Codes), !.
 is_meta(_Name,_Ar,'<meta>false</meta>').
 
 is_volatile(Name,Ar,'\n\t\t<volatile>true</volatile>') :- volatiles(Name/Ar), !.
@@ -119,18 +127,19 @@ is_multifile(Name,Ar,'\n\t\t<multifile>true</multifile>') :- multifiles(Name/Ar)
 is_multifile(_Name,_Ar,'\n\t\t<multifile>false</multifile>').
 
 
-write_predicates2(Name,Ar,Code,Calls,StartLines,EndLines,VC) :-
-    retract(predicates(Name,Ar,Args1,Body1,Calls1,StartLine,EndLine)),
+write_predicates2(Module,Name,Ar,Code,Calls,StartLines,EndLines,VC) :-
+    retract(predicates(Module,Name,Ar,Args1,Body1,Calls1,StartLine,EndLine)),
     bind_args(Args1,VC,VCN),
     bind_args(Body1,VCN,VCN2),
     NewCode = [ Args1, Body1|Code],
     append(Calls,Calls1,NewCalls),
-    write_predicates2(Name,Ar,NewCode,NewCalls,[StartLine|StartLines],[EndLine|EndLines],VCN2).
-write_predicates2(Name,Ar,_Code,Calls,StartLines,EndLines,_VNC) :-
+    write_predicates2(Module,Name,Ar,NewCode,NewCalls,[StartLine|StartLines],[EndLine|EndLines],VCN2).
+write_predicates2(_Module,Name,Ar,_Code,Calls,StartLines,EndLines,_VNC) :-
     is_dynamic(Name,Ar,Dynamic), is_meta(Name,Ar,Meta), is_volatile(Name,Ar,Volatile), is_multifile(Name,Ar,Multifile),
     stream(Stream),
 	escaping_format(Stream,'\t<predicate>\n\t\t<name>"~w"</name>\n\t\t<arity>~w</arity>\n\t\t<startlines>~w</startlines>\n\t\t<endlines>~w</endlines>\n\t\t~w\n\t\t~w ~w ~w\n\t\t<calls>',[Name,Ar,StartLines,EndLines,Dynamic,Meta, Volatile,Multifile]),
-    write_calls(Calls), write(Stream,'\n\t\t</calls>'),
+	setify(Calls, CallsNoDups),
+	write_calls(CallsNoDups), write(Stream,'\n\t\t</calls>'),
 	write(Stream, '\n\t\t<meta_args>'),
 	write_metas(Name/Ar),
 	write(Stream, '</meta_args>'),
@@ -169,27 +178,33 @@ write_xml_representation :-
     write(Stream,'</programm>').
 
 update_calls_all_preds :-
-    findall(pred(Name,Ar,Arguments,Body,Calls,Start,End),
-	    predicates(Name,Ar,Arguments,Body,Calls,Start,End),
+    findall(pred(Module,Name,Ar,Arguments,Body,Calls,Start,End),
+	    predicates(Module,Name,Ar,Arguments,Body,Calls,Start,End),
 	    ListOfAssertedPreds),
     maplist(update_calls,ListOfAssertedPreds).
 
-update_calls(pred(Name,Ar,Arguments,Body,Calls,Start,End)) :-
+update_calls(pred(Module,Name,Ar,Arguments,Body,Calls,Start,End)) :-
     maplist(update_call,Calls,UpdatedCalls),
-    retract(predicates(Name,Ar,Arguments,Body,Calls,Start,End)),
-    assert(predicates(Name,Ar,Arguments,Body,UpdatedCalls,Start,End)).
+    retract(predicates(Module,Name,Ar,Arguments,Body,Calls,Start,End)),
+	setify(UpdatedCalls, UCNoDups),
+    assert(predicates(Module,Name,Ar,Arguments,Body,UCNoDups,Start,End)).
 
 update_call(call(Module,Call,Arity),call(Module2,Call,Arity)) :-
     Module = nil
     -> update_module(Call,Arity,Module2)
     ;  Module2 = Module.
 
+
+setify(L,R) :- setify(L,[],R).
+setify([],A,A).
+setify([H|T],A,R) :- (member(H,T) -> setify(T,A,R); setify(T,[H|A],R)).
+
 update_module('RECURSIVE_CALL',_A,X) :- !, in_module(X).
 update_module(Call,Arity,Module2) :-
     in_module(X), functor(CallAndVar,Call,Arity),
     (predicate_property(X:CallAndVar,built_in) -> Module2 = built_in ;
      predicate_property(X:CallAndVar,imported_from(From)) -> Module2 = From ;
-     predicates(Call,Arity,_,_,_,_,_) -> Module2 = X ;
+     predicates(_,Call,Arity,_,_,_,_,_) -> Module2 = X ;
      imports(ModuleI,Call,Arity) -> Module2 = ModuleI ;
      dynamics(Call/Arity) -> Module2 = 'dynamic predicate';
      otherwise -> Module2 = foo_error).
@@ -344,7 +359,8 @@ analyze((Head :- Body), [LayoutHead | LayoutSub], (Head :- Body)) :-
 	(atom_codes(Fun, "-->") -> Args=[DCG_Name,_Rest], atom_concat(DCG_Name,-->,Name); Name=Fun),				% Analyze dcgs
     flatten([LayoutHead|LayoutSub],[StartLine|FlatLayout]),
     (FlatLayout = [] -> EndLine = StartLine ; last(FlatLayout,EndLine)),
-    assert(predicates(Name,Ar,Args,Body,Calls,StartLine,EndLine)),
+    in_module(Module),
+	assert(predicates(Module,Name,Ar,Args,Body,Calls,StartLine,EndLine)),
     assert(in_clause(Name,Ar)).
 
 
@@ -354,7 +370,8 @@ analyze(Fact, Layout, Fact) :-
    	(atom_codes(Fun, "-->") -> Args=[DCG_Name,_Rest], atom_concat(DCG_Name,-->,Name); Name=Fun),				% Analyze dcgs
  	flatten(Layout,[StartLine|FlatLayout]),
     (FlatLayout = [] -> EndLine = StartLine ; last(FlatLayout,EndLine)),
-    assert(predicates(Name,Ar,Args,'',[],StartLine,EndLine)).
+	in_module(Module),
+    assert(predicates(Module,Name,Ar,Args,'',[],StartLine,EndLine)).
 
 
 analyze_file(FileName):-
